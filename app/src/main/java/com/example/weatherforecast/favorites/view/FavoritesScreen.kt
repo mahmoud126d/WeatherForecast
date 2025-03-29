949c61df37b24124d0725f997f5612452cf04515
mahmoud125d <mhmoudsalah1997@gmail.com>
2025-03-29 17:22:51 +0200

    update favorite Ui

--- app/src/main/java/com/example/weatherforecast/favorites/view/FavoritesScreen.kt
+++ app/src/main/java/com/example/weatherforecast/favorites/view/FavoritesScreen.kt
@@ -1,36 +1,61 @@
 package com.example.weatherforecast.favorites.view
 
+//noinspection UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
 import android.util.Log
-import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.Card
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.weatherforecast.LocationManager
+import com.example.weatherforecast.R
 import com.example.weatherforecast.db.WeatherDataBase
 import com.example.weatherforecast.db.WeatherLocalDataSourceImp
 import com.example.weatherforecast.favorites.viewmodel.FavoritesViewModel
@@ -39,27 +64,8 @@ import com.example.weatherforecast.home.view.RefreshableScreen
 import com.example.weatherforecast.network.CurrentWeatherRemoteDataSourceImpl
 import com.example.weatherforecast.network.RetrofitHelper
 import com.example.weatherforecast.repository.CurrentWeatherRepositoryImpl
-import com.example.weatherforecast.utils.Constants
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.SwipeToDismiss
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.rememberDismissState
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.DismissDirection
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.DismissValue
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.sp
-import com.example.weatherforecast.LocationManager
 import com.example.weatherforecast.repository.LocationRepository
+import com.example.weatherforecast.utils.Constants
 import kotlinx.coroutines.launch
 
 lateinit var favoritesViewModel: FavoritesViewModel
@@ -84,14 +90,12 @@ fun FavoritesScreen(
     favoritesViewModel = viewModel(factory = factory)
     LaunchedEffect(Unit) {
         favoritesViewModel.getAllFavorites()
-        favoritesViewModel.toastEvent.collect { message ->
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 
-        }
     }
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
     Scaffold(
+        containerColor = colorResource(id = R.color.background_color),
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
         },
@@ -100,9 +104,9 @@ fun FavoritesScreen(
                 onClick = {
                     navController.navigate(Constants.FAVORITE_MAP_SCREEN)
                 },
-                containerColor = Color.Blue
+                containerColor = colorResource(R.color.purple_500),
             ) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
+                Icon(Icons.Default.Favorite, contentDescription = "Add",tint = Color.White)
             }
         },
         floatingActionButtonPosition = FabPosition.End
@@ -152,9 +156,14 @@ fun FavoriteColumn(
 ) {
     val favoriteWeatherState = favoritesViewModel.productFavoriteList.collectAsState()
 
-    LazyColumn(modifier = modifier) {
-        items(favoriteWeatherState.value.size) { index ->
-            val item = favoriteWeatherState.value[index]
+    LazyColumn(
+        modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        itemsIndexed(
+            items = favoriteWeatherState.value,
+            key = { _, item -> item.city }
+        ) { index, item ->
             val dismissState = rememberDismissState(
                 confirmStateChange = { dismissValue ->
                     if (dismissValue == DismissValue.DismissedToStart) {
@@ -170,20 +179,21 @@ fun FavoriteColumn(
                 state = dismissState,
                 directions = setOf(DismissDirection.EndToStart),
                 background = {
-//                    Box(
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .background(Color.White)
-//                            .padding(16.dp),
-//                        contentAlignment = Alignment.CenterEnd
-//                    ) {
-//                        Icon(
-//                            imageVector = Icons.Default.Delete,
-//                            contentDescription = "Delete",
-//                            tint = Color.White,
-//                            modifier = Modifier.size(40.dp)
-//                        )
-//                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(Color.Red)
+                            .padding(16.dp),
+                        contentAlignment = Alignment.CenterEnd
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete",
+                            tint = Color.White,
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
                 },
                 dismissContent = {
                     WeatherItem(
@@ -209,12 +219,12 @@ fun WeatherItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(80.dp)
             .clickable { navigate() }
-            .clip(RoundedCornerShape(20.dp))
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(horizontal = 20.dp, vertical = 8.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
@@ -227,8 +237,9 @@ fun WeatherItem(
                 text = fullAddress,
             )
             Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
-                contentDescription = ""
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = "",
+                tint = colorResource(R.color.purple_500)
             )
         }
     }
